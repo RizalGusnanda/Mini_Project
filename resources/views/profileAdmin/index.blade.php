1ab6420acb276af0ad49838fda00e372988435e7
allhusein <2041720040@student.polinema.ac.id>
2023-08-05 12:56:40 +0700

    editseacrhdancornerprofileadmin

--- resources/views/profileAdmin/index.blade.php
+++ resources/views/profileAdmin/index.blade.php
@@ -15,60 +15,61 @@
             </div>
         </div>
 
-<div class="container-xl px-4 mt-4">
-    <hr class="mt-0 mb-4">
-    <div class="row">
-        <div class="col-xl-4">
-            <!-- Profile picture card-->
-            <div class="card mb-4 mb-xl-0">
-                <div class="card-header">Profile Picture</div>
-                <div class="card-body text-center">
-                    <!-- Profile picture image-->
-                    <img class="img-account-profile rounded-circle mb-2" src="http://bootdey.com/img/Content/avatar/avatar1.png" alt="">
-                    <!-- Profile picture help block-->
-                    <div class="small font-italic text-muted mb-4">JPG or PNG no larger than 5 MB</div>
-                    <!-- Profile picture upload button-->
-                    <button class="btn btn-primary" type="button">Upload new image</button>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-8">
-            <!-- Account details card-->
-            <div class="card mb-4">
-                <div class="card-header">Account Details</div>
-                <div class="card-body">
-                    <form>
-                        <!-- Form Group (username)-->
-                        <div class="mb-3">
-                            <label class="small mb-1" for="inputUsername">Username</label>
-                            <input class="form-control" id="inputUsername" type="text" placeholder="Masukan alamat" value="username">
-                        </div>
-                         <!-- Form Group (email address)-->
-                         <div class="mb-3">
-                            <label class="small mb-1" for="inputEmailAddress">Email address</label>
-                            <input class="form-control" id="inputEmailAddress" type="email" placeholder="Enter your email address" value="name@example.com">
-                        </div>
-                        <!-- Form Row        -->
-                        <div class="row gx-3 mb-3">
-                            <!-- Form Group (organization name)-->
-                            <div class="col-md-6">
-                                <label class="small mb-1" for="inputKelamin">Jenis Kelamin</label>
-                                <input class="form-control" id="inputKelamin" type="text" placeholder="Jenis kelamin" value="####">
+                    <div class="container-xl px-4 mt-4">
+                        <hr class="mt-0 mb-4">
+                        <div class="row">
+                        <div class="col-xl-4">
+                        <!-- Profile picture card-->
+                        <div class="card mb-4" style="border-radius: 15px;">
+                            <div class="card-header">Foto Profile</div>
+                            <div class="card-body text-center">
+                                <!-- Profile picture image-->
+                                <img class="img-account-profile rounded-circle mb-3" src="http://bootdey.com/img/Content/avatar/avatar1.png" alt="" style="width: 190px; height: 190px;">
+                                <!-- Profile picture help block-->
+                                <div class="small font-italic text-muted mb-4">JPG or PNG no larger than 5 MB</div>
+                                <!-- Profile picture upload button-->
+                                <button class="btn btn-primary" type="button">Upload new image</button>
                             </div>
-                            <!-- Form Group (location)-->
-                            <div class="col-md-6">
-                                <label class="small mb-1" for="inputLocation">Jenis Kelamin</label>
-                                <input class="form-control" id="inputPhone" type="tel" placeholder="Enter your phone number" value="555-123-4567">
+                        </div>
+                    </div>  
+
+                        <div class="col-xl-8">
+                            <!-- Account details card-->
+                            <div class="card mb-4"  style="border-radius: 15px;">
+                                <div class="card-header">Detail Akun</div>
+                                <div class="card-body">
+                                    <form>
+                                        <!-- Form Group (username)-->
+                                        <div class="mb-3">
+                                            <label class="small mb-1" for="inputUsername">Username</label>
+                                            <input class="form-control" id="inputUsername" type="text" placeholder="Masukan alamat" >
+                                        </div>
+                                        <!-- Form Group (email address)-->
+                                        <div class="mb-3">
+                                            <label class="small mb-1" for="inputEmailAddress">Alamat Email</label>
+                                            <input class="form-control" id="inputEmailAddress" type="email" placeholder="Enter your email address" >
+                                        </div>
+                                        <!-- Form Row        -->
+                                        <div class="row gx-3 mb-3">
+                                            <!-- Form Group (organization name)-->
+                                            <div class="col-md-6">
+                                                <label class="small mb-1" for="inputKelamin">Jenis Kelamin</label>
+                                                <input class="form-control" id="inputKelamin" type="text" placeholder="Jenis kelamin">
+                                            </div>
+                                            <!-- Form Group (location)-->
+                                            <div class="col-md-6">
+                                                <label class="small mb-1" for="inputPhone">Nomor Telepon</label>
+                                                <input class="form-control" id="inputPhone" type="tel" placeholder="Enter your phone number" >
+                                            </div>
+                                        </div>  
+                                        <!-- Save changes button-->
+                                        <button class="btn btn-primary" type="button">Save changes</button>
+                                    </form>
+                                </div>
                             </div>
-                        </div>  
-                        <!-- Save changes button-->
-                        <button class="btn btn-primary" type="button">Save changes</button>
-                    </form>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </div>
-</div>
 
 
                 </div>
