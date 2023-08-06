f1d4cb3b6b5f234f8eb555c9bf5dc54bd0f07709
RizalGusnanda <muchamadrizalgusnanda@gmail.com>
2023-08-06 10:28:39 +0700

    fix conflict

--- resources/views/pengajaran/spesialisasi/create.blade.php
+++ resources/views/pengajaran/spesialisasi/create.blade.php
@@ -4,12 +4,6 @@
     <section class="section">
         <div class="section-header">
             <h1>Table Spesialisasi</h1>
-
-            <div class="section-header-breadcrumb">
-                <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-                <div class="breadcrumb-item"><a href="#">Components</a></div>
-                <div class="breadcrumb-item">Table</div>
-            </div>
         </div>
         <div class="section-body">
             <h2 class="section-title">Tambah Spesialisasi</h2>
@@ -22,19 +16,11 @@
                     <form action="{{ route('spesialisasi.store') }}" method="post">
                         @csrf
                         <div class="form-group">
-
                             <label>Spesialisasi</label>
                             <input type="text" id="nama_spesialisasi" name="nama_spesialisasi"
                                 class="form-control @error('nama_spesialisasi') is-invalid @enderror"
                                 placeholder="Masukan Nama Spesialisasi">
                             @error('nama_spesialisasi')
-
-                            <label>Kelurahan</label>
-                            <input type="text" id="namaSpesialisasi" name="spesialisasi"
-                                class="form-control @error('spesialisasi') is-invalid @enderror"
-                                placeholder="Masukan Nama Spesialisasi" autocomplete="off">
-                            @error('spesialisasi')
-
                                 <div class="invalid-feedback">
                                     {{ $message }}
                                 </div>
